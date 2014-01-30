e5bfb4470b14ec2a84198a4b150d3ea6e7adde0a
Mark Bacalla <mark.bacalla@gmail.com>
2014-01-30 10:10:28 +0800

    Fix email Verification

--- client/main.js
+++ client/main.js
@@ -1,6 +1,17 @@
-var verifyEmail = function () {
+var verifyEmail = function () {	
     return Meteor.user().emails[0].verified;
 };
 
-Template.contact.verifiedEmail = verifyEmail;
-Template.contacts.verifiedEmail = verifyEmail;
+Template.contact.verifiedEmail = verifyEmail
+Template.contacts.verifiedEmail = verifyEmail
+
+Template.verifyEmail.events = {
+	
+	"click .btn-primary": function (e, tmpl) {
+		e.preventDefault();
+
+		Meteor.call('resendVerifaction');
+
+		alert('Email Sent!');
+	}
+}
