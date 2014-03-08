efecee822c6b33b80945459c19787facb7dd97d1
Mark <mark.bacalla@gmail.com>
2014-03-08 18:27:29 +0800

    Upgrade Router using before hooks

---
+++ client/logins.js
@@ -0,0 +1,128 @@
+// Reference: http://blog.benmcmahen.com/post/41741539120/building-a-customized-accounts-ui-for-meteor
+
+var trimInput = function(val) {
+  return val.replace(/^\s*|\s*$/g, "");
+}
+
+var isValidPassword = function(val) {
+   return val.length >= 6 ? true : false; 
+}
+
+  Meteor.autorun(function() {
+    // Whenever this session variable changes, run this function.
+    var message = Session.get('displayMessage');
+    if (message) {
+      var stringArray = message.split('&');
+      alert(stringArray[0]);
+      // ui.notify(stringArray[0], stringArray[1])
+      //   .effect('slide')
+      //   .closable();
+
+      Session.set('displayMessage', null);
+    }
+  });
+
+Template.login.events({
+
+  'submit #login-form' : function(e, t){
+    e.preventDefault();
+    // retrieve the input field values
+    var email = t.find('#login-email').value
+      , password = t.find('#login-password').value;
+
+      // Trim and validate your fields here.... 
+
+      // If validation passes, supply the appropriate fields to the
+      // Meteor.loginWithPassword() function.
+      Meteor.loginWithPassword(email, password, function(err){
+        if (err)
+          // The user might not have been found, or their passwword
+          // could be incorrect. Inform the user that their
+          // login attempt has failed. 
+          console.log(err);
+        else
+          // The user has been logged in.
+        console.log('Success!');
+        Router.go('/');
+      });
+
+       return false; 
+    }
+});
+
+Template.register.events({
+  'submit #register-form' : function(e, t) {
+    e.preventDefault();
+    var email = t.find('#account-email').value
+      , password = t.find('#account-password').value;
+
+      // Trim and validate the input
+    email = trimInput(email);
+
+    if (isValidPassword(password)) {
+        Accounts.createUser({email: email, password : password}, function(err){
+          if (err) {            
+            // Inform the user that account creation failed
+            Session.set('displayMessage', 'Error in Creating User');
+          } else {
+            // Success. Account has been created and the user
+            // has logged in successfully. 
+            Session.set('displayMessage', 'User Successfully Created and Logged In');
+          }
+
+        });
+    } else {
+        alert ('Invalid Password!')
+    }
+
+
+    return false;
+  }
+});
+
+
+  Template.passwordRecovery.helpers({
+    resetPassword : function(t) {
+      return Session.get('resetPassword');
+    }
+  });
+
+
+
+  Template.passwordRecovery.events({
+
+      'submit #recovery-form' : function(e, t) {
+        e.preventDefault()
+        var email = trimInput(t.find('#recovery-email').value)
+
+        if (isNotEmpty(email) && isEmail(email)) {
+          Session.set('loading', true);
+          Accounts.forgotPassword({email: email}, function(err){
+          if (err)
+            Session.set('displayMessage', 'Password Reset Error & Doh')
+          else {
+            Session.set('displayMessage', 'Email Sent & Please check your email.')
+          }
+          Session.set('loading', false);
+        });
+        }
+        return false; 
+      },
+
+      'submit #new-password' : function(e, t) {
+        e.preventDefault();
+        var pw = t.find('#new-password-password').value;
+        if (isNotEmpty(pw) && isValidPassword(pw)) {
+          Session.set('loading', true);
+          Accounts.resetPassword(Session.get('resetPassword'), pw, function(err){
+            if (err)
+              Session.set('displayMessage', 'Password Reset Error & Sorry');
+            else {
+              Session.set('resetPassword', null);
+            }
+            Session.set('loading', false);
+          });
+        }
+      return false; 
+      }
+  });
--- client/router.js
+++ client/router.js
@@ -1,3 +1,61 @@
+// Reference: http://www.manuel-schoebel.com/blog/meteorjs-iron-router-filters-before-and-after-hooks
+
+
+var IR_Filters = {
+    // All standard subscriptions you need before anything works
+    // the .wait() makes sure that it continues only if the subscription
+    // is ready and the data available
+    // Use: global
+    baseSubscriptions: function() {
+        this.subscribe('userData').wait();
+    },
+    // show login if a guest wants to access private areas
+    // Use: {only: [privateAreas] }
+    isLoggedIn: function() {
+        if (!(Meteor.loggingIn() || Meteor.user())) {
+          //Notify.setError(__('Please login.')); // some custom packages
+          this.render('login');
+          this.stop();
+        }
+    },
+    // make sure to scroll to the top of the page on a new route
+    // Use: global
+    scrollUp: function() {
+        $('body,html').scrollTop(0);
+    },
+    // if this route depends on data, show the NProgess loading indicator
+    // http://ricostacruz.com/nprogress/
+    // Use: global
+    startNProgress: function() {
+        if (_.isFunction(this.data)) {
+          NProgress.start();
+        }
+    },
+    // tell google analytics that a page was viewed
+    // e.g. https://github.com/datariot/meteor-ganalytics
+    // Use: global
+    pageview: function() {
+        GAnalytics.pageview(this.path);
+    },
+    // only show route if you are an admin
+    // using https://github.com/alanning/meteor-roles
+    // Use: {only: [adminAreas]}
+    isAdmin: function() {
+        if (!Roles.userIsInRole(Meteor.userId(), ['admin'])) {
+          this.render('login');
+          this.stop();
+        }
+    },
+    // animate old content out using
+    // http://daneden.github.io/animate.css/
+    // Use: global
+    animateContentOut: function() {
+        $('#content').removeClass("animated fadeIn fadeInRight");
+        $('footer').addClass("hide");
+    }
+}
+
+
 Router.configure({
     layoutTemplate: 'layout'
 });
@@ -15,4 +73,10 @@ Router.map(function() {
         template: 'contactList'
     });
 
+    this.route("register", {
+        path: "/register",
+        template: 'register'
+    })
 });
+
+Router.before(IR_Filters.isLoggedIn, { except: ['register']});
